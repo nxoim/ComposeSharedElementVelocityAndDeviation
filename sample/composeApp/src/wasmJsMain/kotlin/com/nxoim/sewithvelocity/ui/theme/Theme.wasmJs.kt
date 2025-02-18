d0b7c986dce7c285205fc19141ddb595fbcecaa6
nxoim <ghite@nxoim.com>
2025-02-18 20:45:32 +0100

    theme fox for wasm

--- sample/composeApp/src/wasmJsMain/kotlin/com/nxoim/sewithvelocity/ui/theme/Theme.wasmJs.kt
+++ sample/composeApp/src/wasmJsMain/kotlin/com/nxoim/sewithvelocity/ui/theme/Theme.wasmJs.kt
@@ -1,13 +1,19 @@
 package com.nxoim.sewithvelocity.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
 @Composable
 internal actual fun SampleTheme(content: @Composable () -> Unit) {
     MaterialTheme(
-        DarkColorScheme,
+        if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme(),
         typography = Typography,
-        content = content
+        content = {
+            Surface(content = content)
+        }
     )
 }
