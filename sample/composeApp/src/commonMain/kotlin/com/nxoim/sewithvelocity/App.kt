6b81c5683539db1c1b685058e96de97ccaeb5fb5
nxoim <ghite@nxoim.com>
2025-02-19 05:08:57 +0100

    added a grid example along with some explanations

--- sample/composeApp/src/commonMain/kotlin/com/nxoim/sewithvelocity/App.kt
+++ sample/composeApp/src/commonMain/kotlin/com/nxoim/sewithvelocity/App.kt
@@ -1,5 +1,6 @@
 package com.nxoim.sewithvelocity
 
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
@@ -16,7 +17,14 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridItemSpan.Companion.FullLine
+import androidx.compose.foundation.lazy.staggeredgrid.itemsIndexed
+import androidx.compose.foundation.lazy.staggeredgrid.rememberLazyStaggeredGridState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
@@ -31,9 +39,11 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.round
 import com.nxoim.sewithvelocity.sharedElement.SharedTransitionLayout
+import com.nxoim.sewithvelocity.sharedElement.SharedTransitionScope
 import kotlin.random.Random
 
 
@@ -50,50 +60,103 @@ fun App() {
     var stiffness by remember { mutableStateOf(350f) }
     var dampingRatio by remember { mutableStateOf(0.9f) }
     var visibilityThreshold by remember { mutableStateOf(1.5f) }
+    var isGrid by remember { mutableStateOf(false) }
+    var minGridItemSize by remember { mutableStateOf(94.dp) }
+
+    @Composable
+    fun SharedTransitionScope.ColoredBoxItem(index: Int, color: Color, inGrid: Boolean) {
+        Box(
+            modifier = Modifier
+                .aspectRatio(1.5f)
+                .fillMaxSize()
+                .padding(8.dp)
+                .sharedElementWithCallerManagedVisibility(
+                    rememberSharedContentState(key = "box_$index"),
+                    visible = selectedIndex != index && inGrid == isGrid,
+                    boundsTransform = { _, _ ->
+                        spring(
+                            stiffness = stiffness,
+                            dampingRatio = dampingRatio,
+                            visibilityThreshold = Rect(
+                                visibilityThreshold,
+                                visibilityThreshold,
+                                visibilityThreshold,
+                                visibilityThreshold
+                            )
+                        )
+                    }
+                )
+                .background(color, RoundedCornerShape(16.dp))
+                .clickable { selectedIndex = index }
+        )
+    }
+
+    // hoisting states outside of animated content
+    val lazyListState = rememberLazyListState()
+    val lazyStaggeredGridState = rememberLazyStaggeredGridState()
 
     // customized implementation of shared transition things
     SharedTransitionLayout {
-        LazyColumn() {
-            item {
-                AnimationSettingsContainer(
-                    velocityMultiplier = velocityMultiplier,
-                    onVelocityMultiplierChange = { velocityMultiplier = it },
-                    stiffness = stiffness,
-                    onStiffnessChange = { stiffness = it },
-                    dampingRatio = dampingRatio,
-                    onDampingRatioChange = { dampingRatio = it },
-                    visibilityThreshold = visibilityThreshold,
-                    onVisibilityThresholdChange = { visibilityThreshold = it }
-                )
-            }
+        // AnimatedContent is a requirement for the animation
+        // to start as it lets the initial content stay in composition,
+        // which is necessary due to how state is managed in
+        // shared transition scope, specifically "onStateRemoved"
+        // function. Were this to be done using just an if
+        // statement - the state would get removed before
+        // the grid appeared, causing loss of data about the element.
 
-            itemsIndexed(colors) { index, color ->
-                Box(
-                    modifier = Modifier
-                        .aspectRatio(1.5f)
-                        .fillMaxSize()
-                        .padding(8.dp)
-                        .sharedElementWithCallerManagedVisibility(
-                            rememberSharedContentState(key = "box_$index"),
-                            visible = selectedIndex != index,
-                            boundsTransform = { _, _ ->
-                                spring(
-                                    stiffness = stiffness,
-                                    dampingRatio = dampingRatio,
-                                    visibilityThreshold = Rect(
-                                        visibilityThreshold,
-                                        visibilityThreshold,
-                                        visibilityThreshold,
-                                        visibilityThreshold
-                                    )
-                                )
-                            }
+        // ...and also because new elements will appear nicely on screen
+        AnimatedContent(isGrid) {
+            if (it) {
+                LazyVerticalStaggeredGrid(
+                    state = lazyStaggeredGridState,
+                    columns = StaggeredGridCells.Adaptive(minGridItemSize),
+                ) {
+                    item(span = FullLine) {
+                        AnimationSettingsContainer(
+                            velocityMultiplier = velocityMultiplier,
+                            onVelocityMultiplierChange = { velocityMultiplier = it },
+                            stiffness = stiffness,
+                            onStiffnessChange = { stiffness = it },
+                            dampingRatio = dampingRatio,
+                            onDampingRatioChange = { dampingRatio = it },
+                            visibilityThreshold = visibilityThreshold,
+                            onVisibilityThresholdChange = { visibilityThreshold = it },
+                            minGridItemSize = minGridItemSize,
+                            onMinGridItemSizeChange = { minGridItemSize = it },
+                            onSwitchView = { isGrid = false }
                         )
-                        .background(color, RoundedCornerShape(16.dp))
-                        .clickable { selectedIndex = index }
-                )
+                    }
+
+                    itemsIndexed(colors) { index, color ->
+                        ColoredBoxItem(index, color, true)
+                    }
+                }
+            } else {
+                LazyColumn(state = lazyListState) {
+                    item {
+                        AnimationSettingsContainer(
+                            velocityMultiplier = velocityMultiplier,
+                            onVelocityMultiplierChange = { velocityMultiplier = it },
+                            stiffness = stiffness,
+                            onStiffnessChange = { stiffness = it },
+                            dampingRatio = dampingRatio,
+                            onDampingRatioChange = { dampingRatio = it },
+                            visibilityThreshold = visibilityThreshold,
+                            onVisibilityThresholdChange = { visibilityThreshold = it },
+                            minGridItemSize = minGridItemSize,
+                            onMinGridItemSizeChange = { minGridItemSize = it },
+                            onSwitchView = { isGrid = true },
+                        )
+                    }
+
+                    itemsIndexed(colors) { index, color ->
+                        ColoredBoxItem(index, color, false)
+                    }
+                }
             }
         }
+
 
         val scrimAlpha by animateFloatAsState(
             if (selectedIndex == null) 0f else 1f,
@@ -143,7 +206,8 @@ fun App() {
                         .fillMaxSize()
                         .draggable2D(
                             draggable2DState,
-                            onDragStopped = { selectedIndex = null }
+                            onDragStopped = { selectedIndex = null
+                            }
                         )
                 )
             }
@@ -151,6 +215,7 @@ fun App() {
     }
 }
 
+@OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
 private fun AnimationSettingsContainer(
     velocityMultiplier: Float,
@@ -160,7 +225,10 @@ private fun AnimationSettingsContainer(
     dampingRatio: Float,
     onDampingRatioChange: (Float) -> Unit,
     visibilityThreshold: Float,
-    onVisibilityThresholdChange: (Float) -> Unit
+    onVisibilityThresholdChange: (Float) -> Unit,
+    minGridItemSize: Dp,
+    onMinGridItemSizeChange: (Dp) -> Unit,
+    onSwitchView: () -> Unit
 ) {
     Column(
         modifier = Modifier
@@ -199,9 +267,24 @@ private fun AnimationSettingsContainer(
             label = "Visibility Threshold: $visibilityThreshold",
             value = visibilityThreshold,
             onValueChange = onVisibilityThresholdChange,
-            valueRange = 0.0f..10f,
+            valueRange = 0f..10f,
             steps = 99
         )
+
+        LabeledSlider(
+            label = "Minimum Grid Item Size: $minGridItemSize",
+            value = minGridItemSize.value,
+            onValueChange = { onMinGridItemSizeChange(it.dp) },
+            valueRange = 0f ..300f,
+            steps = 29
+        )
+
+        FilledTonalButton(
+            onSwitchView,
+            Modifier.fillMaxWidth()
+        ) {
+            Text("Switch View")
+        }
     }
 }
 
